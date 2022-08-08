e4023b2395b9b7d1cf4038c6dbf4d2cd2155414e
Sbatkov <sergeibatkov@bk.ru>
2022-08-08 11:28:48 +0300

    display stripe pricing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
+  get "pricing", to: "static_pages#pricing"
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
