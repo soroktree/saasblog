d3ecf376eb054d13cbff6555ffe4b437a0712672
Sbatkov <sergeibatkov@bk.ru>
2022-08-08 20:38:19 +0300

    current user can select plan

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,17 +1,18 @@
 class CheckoutController < ApplicationController
   def create
     @session = Stripe::Checkout::Session.create({
-                                       success_url: pricing_url,
-                                       cancel_url: posts_url,
+                                       customer: current_user.stripe_customer_id,
+                                       success_url: posts_url,
+                                       cancel_url: pricing_url,
                                        line_items: [
-                                         { price: 'price_1LUF8cLmMqCJ6TiKtnaTYGqY', quantity: 1 }
+                                         { price: params[:price], quantity: 1 }
                                        ],
                                        payment_method_types: [
                                          'card'
                                        ],
                                        mode: 'subscription'
                                      })  
-                                                              
+
     redirect_to @session.url, allow_other_host: true
 
     rescue Stripe::InvalidRequestError => error
