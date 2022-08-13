45fd519770c5c92f2d87ef1b87f59797cb28993b
Sbatkov <sergeibatkov@bk.ru>
2022-08-14 00:39:12 +0300

    stripe rb api key name change and dig

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-    :publishable_key => Rails.application.credentials.stripe[:stripe_publishable_key],    
-    :secret_key => Rails.application.credentials.stripe[:stripe_secret_key]    
-  }    
-  Stripe.api_key = Rails.application.credentials.stripe[:stripe_secret_key]
+  :publishable_key => Rails.application.credentials.dig(:stripe, :stripe_publishable_key),    
+  :secret_key => Rails.application.credentials.dig(:stripe,:stripe_secret_key)    
+}    
+Stripe.api_key = Rails.application.credentials.dig(:stripe, :stripe_secret_key)
