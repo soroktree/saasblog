e55ef7b5e74015b796762237e01afd55e36fbe1f
Sbatkov <sergeibatkov@bk.ru>
2022-08-13 22:34:14 +0300

    heroku try 6

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Saasblog
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
@@ -18,6 +19,5 @@ module Saasblog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.assets.initialize_on_precompile = false
   end
 end
